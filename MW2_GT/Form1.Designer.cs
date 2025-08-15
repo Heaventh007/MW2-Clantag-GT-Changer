0d886a372af5652591190c2c3ca6fed846d13bcd
Heaventh <115350143+Heaventh007@users.noreply.github.com>
2025-08-15 18:05:50 +0200

    Added main project files.

---
+++ MW2_GT/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MW2_GT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clantag";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Gamertag";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 100);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MW2 Schtuff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MW2_GT/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using XDevkit;
+using JRPC_Client;
+
+namespace MW2_GT
+{
+    public partial class Form1 : Form
+    {
+        private IXboxConsole Xbox360;
+
+        public Form1() => InitializeComponent();
+        bool CL_InGame() => Xbox360.ReadBool(Xbox360.ReadUInt32(0x825E21F0) + 0x8);
+
+        public void SetInGameInfo(string Gamertag, string Clantag) {
+            Gamertag = Gamertag.Split('\0')[0];
+            Clantag = Clantag.Split('\0')[0];
+
+            string XUIDString = Xbox360.ReadString(0x838BA850, 0x12).Split('\0')[0];
+
+            int NatType = Xbox360.ReadInt32(0x8253A508);
+
+            int Rank = Xbox360.Call<int>(0x822C8688, 0);
+            int Prestige = Xbox360.Call<int>(0x822C86E8, 0);
+
+            Xbox360.CallVoid(0x82224990, 0, $"cmd userinfo \"name\\{Gamertag}\\clanAbbrev\\{Clantag}\\xuid\\{XUIDString.ToLower()}\\natType\\{NatType}\\rank\\{Rank}\\prestige\\{Prestige}\"");
+        }
+
+        public void SetGamertag(string Gamertag) {
+            if (Gamertag.Length >= 16) {
+                MessageBox.Show("Oi pal, gamertag is too long.");
+                return;
+            }
+
+            if (CL_InGame())
+                SetInGameInfo(Gamertag, Xbox360.ReadString(0x82687060, 4));
+
+            Xbox360.WriteString(0x838BA824, Gamertag);
+        }
+
+        public void SetClantag(string Clantag) {
+            if (Clantag.Length > 4) {
+                MessageBox.Show("Oi pal, clantag is too long.");
+                return;
+            }
+
+            if (CL_InGame()) 
+                SetInGameInfo(Xbox360.ReadString(0x838BA824, 16), Clantag);
+
+            Xbox360.WriteString(0x82687060, Clantag);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (Xbox360.Connect(out Xbox360)) {
+                MessageBox.Show("Connected to console.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (Xbox360 != null) {
+                SetClantag(textBox1.Text);
+                Xbox360.XNotify("Clantag set!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            if (Xbox360 != null) {
+                SetGamertag(textBox2.Text);
+                Xbox360.XNotify("Gamertag set!");
+            }
+        }
+    }
+}
